7a434f190445487f40555f8a11f2fa81d96d564e
Son of Forehead <jpstoner@icloud.com>
2024-06-02 16:12:37 -0600

    Done

--- Load Levels/src/Bonemenu.cs
+++ Load Levels/src/Bonemenu.cs
@@ -2,25 +2,56 @@
 using BoneLib.BoneMenu;
 using UnityEngine;
 using LevelLoader;
+using BoneLib.Notifications;
+using MelonLoader;
 
 namespace LoadLevels
 {
     public static class LoaderBonemenu
     {
         public static MenuCategory mainCategory;
+        public static Notification invalidNotification;
+
         public static void CreateBonemenu()
         {
-            mainCategory = MenuManager.CreateCategory("<color=#ff0000>L<color=#ffa500>e<color=#300000>v<color=#ffff00>e<color=#008000>l<color=#ff0000> L<color=#ffa500>o<color=#300000>a<color=#ffff00>d<color=#008000>e<color=#0000ff>r", Color.yellow);
+            mainCategory = MenuManager.CreateCategory("LevelLoader", Color.blue);
 
-            mainCategory.CreateFunctionElement(LoadLevelsMain.level1.Value, Color.red, LoadLevelsMain.LoadLevel1);
+            mainCategory.CreateFunctionElement("`Default` will load Void G114.", Color.red, () => { });
+            mainCategory.CreateFunctionElement("Reload Preferences", Color.yellow, ReloadPrefs);
 
-            mainCategory.CreateFunctionElement(LoadLevelsMain.level2.Value, Color.yellow, LoadLevelsMain.LoadLevel2);
+            mainCategory.CreateFunctionElement(LoadLevelsMain.level1.Value, Color.gray, LoadLevelsMain.LoadLevel1, "Are you sure?");
 
-            mainCategory.CreateFunctionElement(LoadLevelsMain.level3.Value, Color.green, LoadLevelsMain.LoadLevel3);
+            mainCategory.CreateFunctionElement(LoadLevelsMain.level2.Value, Color.gray, LoadLevelsMain.LoadLevel2, "Are you sure?");
 
-            mainCategory.CreateFunctionElement(LoadLevelsMain.level4.Value, Color.blue, LoadLevelsMain.LoadLevel4);
+            mainCategory.CreateFunctionElement(LoadLevelsMain.level3.Value, Color.gray, LoadLevelsMain.LoadLevel3, "Are you sure?");
 
-            mainCategory.CreateFunctionElement(LoadLevelsMain.level5.Value, Color.black, LoadLevelsMain.LoadLevel5);
+            mainCategory.CreateFunctionElement(LoadLevelsMain.level4.Value, Color.gray, LoadLevelsMain.LoadLevel4, "Are you sure?");
+
+            mainCategory.CreateFunctionElement(LoadLevelsMain.level5.Value, Color.gray, LoadLevelsMain.LoadLevel5, "Are you sure?");
+        }
+
+        public static void ReloadPrefs()
+        {
+            mainCategory.Elements.Clear();
+            LoadLevelsMain.barcode1 = LoadLevelsMain.level1.Value;
+            LoadLevelsMain.barcode2 = LoadLevelsMain.level2.Value;
+            LoadLevelsMain.barcode3 = LoadLevelsMain.level3.Value;
+            LoadLevelsMain.barcode4 = LoadLevelsMain.level4.Value;
+            LoadLevelsMain.barcode5 = LoadLevelsMain.level5.Value;
+            CreateBonemenu();
+        }
+
+        public static void InvalidBarcode()
+        {
+            MelonLogger.Warning("Invalid Barcode!");
+
+            invalidNotification = new Notification();
+            invalidNotification.Title = "Level Loader";
+            invalidNotification.Message = "Invalid Barcode!";
+            invalidNotification.Type = NotificationType.Error;
+            invalidNotification.PopupLength = 3;
+
+            Notifier.Send(invalidNotification);
         }
     }
 }
--- Load Levels/src/Main.cs
+++ Load Levels/src/Main.cs
@@ -2,6 +2,7 @@
 using SLZ.Marrow.SceneStreaming;
 using LoadLevels;
 using SLZ.Marrow.Warehouse;
+using SLZ.Bonelab;
 
 namespace LevelLoader
 {
@@ -42,53 +43,84 @@ namespace LevelLoader
             barcode5 = level5.Value;
         }
 
-        public static void CreateMelonPrefs() // melon prefs aint... preferencing...???
+        public static void CreateMelonPrefs()
         {
             mainPrefCategory = MelonPreferences.CreateCategory("LevelLoader");
-            level1 = mainPrefCategory.CreateEntry<string>("LVL_1:", "VoidG114");
-            level2 = mainPrefCategory.CreateEntry<string>("LVL_2:", "VoidG114");
-            level3 = mainPrefCategory.CreateEntry<string>("LVL_3:", "VoidG114");
-            level4 = mainPrefCategory.CreateEntry<string>("LVL_4:", "VoidG114");
-            level5 = mainPrefCategory.CreateEntry<string>("LVL_5:", "VoidG114");
+            level1 = mainPrefCategory.CreateEntry<string>("LVL_1:", "default");
+            level2 = mainPrefCategory.CreateEntry<string>("LVL_2:", "default");
+            level3 = mainPrefCategory.CreateEntry<string>("LVL_3:", "default");
+            level4 = mainPrefCategory.CreateEntry<string>("LVL_4:", "default");
+            level5 = mainPrefCategory.CreateEntry<string>("LVL_5:", "default");
         }
 
         public static void LoadLevel1()
         {
-            if(barcode1.Contains("." + "." + ".")) // pls make work
+            if(barcode1.Contains(".Level."))
             {
                 MelonLogger.Msg("Valid Level Barcode!");
+                SceneStreamer.Load(barcode1);
+                MelonLogger.Msg("Loading: " + barcode1 + "...");
             }
             else
             {
-                MelonLogger.Warning("Invalid Level Barcode!");
+                LoaderBonemenu.InvalidBarcode();
             }
-
-            SceneStreamer.Load(barcode1);
-            MelonLogger.Msg("Loading: " + barcode1 + "...");
         }
 
         public static void LoadLevel2()
         {
-            SceneStreamer.Load(barcode2);
-            MelonLogger.Msg("Loading: " + barcode1 + "...");
+            if (barcode2.Contains(".Level."))
+            {
+                MelonLogger.Msg("Valid Level Barcode!");
+                SceneStreamer.Load(barcode2);
+                MelonLogger.Msg("Loading: " + barcode2 + "...");
+            }
+            else
+            {
+                LoaderBonemenu.InvalidBarcode();
+            }
         }
 
         public static void LoadLevel3()
         {
-            SceneStreamer.Load(barcode3);
-            MelonLogger.Msg("Loading: " + barcode1 + "...");
+            if (barcode3.Contains(".Level."))
+            {
+                MelonLogger.Msg("Valid Level Barcode!");
+                SceneStreamer.Load(barcode3);
+                MelonLogger.Msg("Loading: " + barcode3 + "...");
+            }
+            else
+            {
+                LoaderBonemenu.InvalidBarcode();
+            }
         }
 
         public static void LoadLevel4()
         {
-            SceneStreamer.Load(barcode4);
-            MelonLogger.Msg("Loading: " + barcode1 + "...");
+            if (barcode4.Contains(".Level."))
+            {
+                MelonLogger.Msg("Valid Level Barcode!");
+                SceneStreamer.Load(barcode4);
+                MelonLogger.Msg("Loading: " + barcode4 + "...");
+            }
+            else
+            {
+                LoaderBonemenu.InvalidBarcode();
+            }
         }
 
         public static void LoadLevel5()
         {
-            SceneStreamer.Load(barcode5);
-            MelonLogger.Msg("Loading: " + barcode1 + "...");
+            if (barcode5.Contains(".Level."))
+            {
+                MelonLogger.Msg("Valid Level Barcode!");
+                SceneStreamer.Load(barcode5);
+                MelonLogger.Msg("Loading: " + barcode5 + "...");
+            }
+            else
+            {
+                LoaderBonemenu.InvalidBarcode();
+            }
         }
     }
 }
